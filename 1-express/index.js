772afc9ab48e13b404d8715c6b9c81ef823b014d
Yuriy <emailworking@ukr.net>
2024-04-13 12:43:10 +0300

    USERS: created a route and connected the view

--- 1-express/index.js
+++ 1-express/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express();
 
+// init handlebars
+const hbs = require('express-handlebars');
+app.engine("hbs", hbs.engine({ extname: ".hbs" }));
+app.set("view engine", "hbs");
+
+
 // route root
 app.get('/', function (req, res) {
     res.send('Hello world :-)');
+});
+
+app.get('/users', function (req, res) {
+    //res.send('View Users');
+    res.render("users", {
+        title: "List USERS demo",
+        users: ['user01', 'user02', 'user03']
+    })
 });
 
 // run server
--- 1-express/readme.md
+++ 1-express/readme.md
@@ -47,6 +47,19 @@ npm i express express-handlebars mongoose
 
 index.js
 
-```
+```javascript
+const express = require('express');
+const app = express();
 
+// route root
+app.get('/', function (req, res) {
+  res.send('Hello world :-)');
+});
+
+// run server
+const APP_PORT = 8080;
+app.listen(APP_PORT, function () {
+  console.log('Listen port :', APP_PORT);
+  console.log('Server Run ...');
+});
 ```
